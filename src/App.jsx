de800ee6e07c58a755885d5306aa796645d588c8
M. Rafly Saputra <966raflisaputra@gmail.com>
2024-11-26 14:46:17 +0700

    mengubah tampilan menjadi full

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ const App = () => {
   
   return (
     <>
-      <main className="mx-auto min-w-80 w-[90%] max-w-[90%] relative border overflow-hidden" style={{height: "93%"}}>
+      <main className="mx-auto min-w-80 w-[100%] md:max-w-[90%] relative border overflow-hidden" style={{height: "93%"}}>
         <header className="p-3 rounded-md shadow bg-blue-800 flex justify-between font-tillana text-slate-300">
           <h1 className="text-xl">{"</>"} RafAi</h1>
           <button id="btn-remove-chat" type="submit" className="text-sm p-2 bg-red-600 rounded-md flex items-center gap-2 disabled:opacity-50 disabled:cursor-not-allowed" onClick={handleClearChat}>Hapus <i className="bx bx-message"></i></button>
