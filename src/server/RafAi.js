dc9157cb84cd1c01e853c3a4a77eafef6a3d0f8d
M. Rafly Saputra <966raflisaputra@gmail.com>
2024-11-27 00:16:33 +0700

    mengubah tampilan

--- src/server/RafAi.js
+++ src/server/RafAi.js
@@ -6,10 +6,13 @@ const { apikey ,prompt } = {
 };
 const RafAi = async (msg, session) => {
     try {
-        const { data }  = await axios(`https://kizhbotz.online/api/luminai?message=${msg}&sifat=${prompt}&session=${session}&apikey=${apikey}`);
+        const { data }  = await axios(`https://kizhbotz.online/api/luminai?message=${msg}&sifat=${prompt}&session=${session}&apikey=${apikey}`, {
+            timeout: 10000
+        });
         return data.data.response;
     } catch(err) {
         console.log("Error RafAi ", err);
+        return "Maaf RafAi tidak dapat merespon, silahkan coba lagi";
     }
 }
 
