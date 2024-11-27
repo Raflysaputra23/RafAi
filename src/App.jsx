c6335ea588f7ce11772b10479359107c00f1c0eb
M. Rafly Saputra <966raflisaputra@gmail.com>
2024-11-27 13:38:34 +0700

    memperbaiki bug

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,9 @@ const App = () => {
   const [ chat, setChat ] = useState(localStorage.getItem("chat") !== null ? JSON.parse(localStorage.getItem("chat")) : []);
   const [ mode, setMode ] = useState("light");
   const [ text, setText ] = useState("");
+  const [ record, setRecord ] = useState(true);
   const [ recording, setRecording ] = useState(Recorder());
+  
   // eslint-disable-next-line no-unused-vars
 
   useEffect(() => {
@@ -99,53 +101,53 @@ const App = () => {
     e.target.setAttribute('disabled','true');
   }
 
-  const StartRecording = (e) => {
+  const handleRecording = (e) => {
     e.preventDefault();
-    console.log("Start recording...")
+    if(record) {
+      setRecord(false);
+      recording.start();
+      console.log("Start recording...")
 
-    recording.start();
-    const button = document.getElementById("recording");
-    button.classList.replace('text-slate-100','text-red-500');
-  }
-  
-  const StopRecording = (e) => {
-    e.preventDefault();
-    recording.stop();
+      const button = document.getElementById("recording");
+      button.classList.replace('text-slate-100','text-red-500');
+    } else {
+      setRecord(true);
+      recording.stop();
+      console.log("Stop recording...");
 
-    const session = localStorage.getItem("session");
-    let finalText = "";
-    console.log("Stop recording...");
+      const session = localStorage.getItem("session");
 
-    recording.onresult = async (event) => {
-      const text = event.results[event.results.length - 1][0].transcript;
-      if(event.results[event.results.length - 1].isFinal && text !== finalText) {
-        setChat((prevChat) => [...prevChat,{role: "user", content: text}]);
-        setChat((prevChat) => [...prevChat,{role: "assistant", content: (<svg className="animate-spin h-5 w-5 border-r-2 border-b-2 border-white rounded-full" viewBox="0 0 24 24"></svg>)}]);
+      recording.onresult = async (event) => {
+        const text = event.results[event.results.length - 1][0].transcript;
+        console.log(text);
+        if(event.results[event.results.length - 1].isFinal) {
+          setChat((prevChat) => [...prevChat,{role: "user", content: text}]);
+          setChat((prevChat) => [...prevChat,{role: "assistant", content: (<svg className="animate-spin h-5 w-5 border-r-2 border-b-2 border-white rounded-full" viewBox="0 0 24 24"></svg>)}]);
 
-        const response = await RafAi(text, session);
-        setChat((prevChat) => [...prevChat.slice(0, -1),{role: "assistant", content: response}]);
-        setChat((prevChat) => {
-          localStorage.setItem("chat", JSON.stringify(prevChat));
-          return prevChat;
-        });
+          const response = await RafAi(text, session);
+          setChat((prevChat) => [...prevChat.slice(0, -1),{role: "assistant", content: response}]);
+          setChat((prevChat) => {
+            localStorage.setItem("chat", JSON.stringify(prevChat));
+            return prevChat;
+          });
+        }
       }
+
+      recording.onerror = (event) => {
+        console.log(event.error);
+      }
+
+      const button = document.getElementById("recording");
+      button.classList.replace('text-red-500','text-slate-100');
     }
-
-    recording.onerror = (event) => {
-      console.log(event.error);
-    }
-
-    const button = document.getElementById("recording");
-    button.classList.replace('text-red-500','text-slate-100');
-
   }
-  
+
   return (
     <>
       <main className="mx-auto min-w-80 w-[100%] md:max-w-[90%] relative border overflow-hidden bg-slate-200 text-slate-900 dark:text-slate-200 dark:bg-slate-800" style={{height: "95%"}}>
         <Header handleClearChat={handleClearChat} handleMode={handleMode} mode={mode} />
         <Body chat={chat} text={text}/>
-        <Footer handleChat={handleChat} handleInput={handleInput} StartRecording={StartRecording} StopRecording={StopRecording} />
+        <Footer handleChat={handleChat} handleInput={handleInput} handleRecording={handleRecording} />
       </main>
     </>
   )
--- src/template/Footer.jsx
+++ src/template/Footer.jsx
@@ -1,13 +1,13 @@
 
 
 // eslint-disable-next-line react/prop-types
-const Footer = ({ handleChat, handleInput, StartRecording, StopRecording }) => {
+const Footer = ({ handleChat, handleInput, handleRecording }) => {
     return (
         <footer className="absolute bottom-0 left-0 right-0 p-3 rounded-md shadow bg-slate-200 font-poppins text-slate-700 dark:bg-slate-900 dark:text-slate-200">
           <form action="" className="flex gap-2" onSubmit={handleChat}>
             <input id="input" type="text" name="chat" className="w-full p-3 rounded-md outline-none border-none bg-slate-100 dark:bg-slate-800" onChange={handleInput} placeholder="Ketik pesan..." />
             <button id="submit" type="submit" className="p-1 px-3 rounded-md bg-blue-800 text-slate-100 disabled:opacity-50 disabled:cursor-not-allowed" disabled><i className="bx bx-send text-2xl"></i></button>
-            <button id="recording" type="button" className="p-1 px-3 rounded-md bg-blue-800 text-slate-100 bx bx-microphone text-2xl" onTouchStart={StartRecording} onTouchEnd={StopRecording}></button>
+            <button id="recording" type="button" className="p-1 px-3 rounded-md bg-blue-800 text-slate-100 bx bx-microphone text-2xl" onClick={handleRecording}></button>
           </form>  
         </footer>
     );
